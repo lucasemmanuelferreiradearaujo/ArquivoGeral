1ef6248458a819e2efca918ad1740f1f1661dbcc
Lucas Emmanuel <63796828+lucasemmanuelferreiradearaujo@users.noreply.github.com>
2023-02-08 01:11:10 -0300

    day 4

--- code/main.py
+++ code/main.py
@@ -1,29 +1,91 @@
-from datetime import datetime
+import PySimpleGUI as sg
+import sqlite3
 
-class Documento:
-    def __init__(self, local_guarda, num_prateleira, num_caixa, num_dossie, num_registro, data_distribuicao, num_volume, ano_arquivamento, proveniencia, assunto, especie, tipologia, requerente, observacao, tipo_arquivo):
-        self.local_guarda = local_guarda
-        self.num_prateleira = num_prateleira
-        self.num_caixa = num_caixa
-        self.num_dossie = num_dossie
-        self.num_registro = num_registro
-        self.data_distribuicao = data_distribuicao
-        self.num_volume = num_volume
-        self.ano_arquivamento = ano_arquivamento
-        self.proveniencia = proveniencia
-        self.assunto = assunto
-        self.especie = especie
-        self.tipologia = tipologia
-        self.requerente = requerente
-        self.observacao = observacao
-        self.tipo_arquivo = tipo_arquivo
-        self.data_cadastro = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    def __str__(self):
-        return f'Local de Guarda: {self.local_guarda}\nN° de Prateleira: {self.num_prateleira}\nN° de Caixa: {self.num_caixa}\nN° de Dossiê: {self.num_dossie}\nN° de Registro: {self.num_registro}\nData de Distribuição: {self.data_distribuicao}\nNúmero do Volume: {self.num_volume}\nAno do Arquivamento: {self.ano_arquivamento}\nProveniência: {self.proveniencia}\nAssunto: {self.assunto}\nEspécie: {self.especie}\nTipologia: {self.tipologia}\nRequerente: {self.requerente}\nObservação: {self.observacao}\nTipo do Arquivo: {self.tipo_arquivo}\nData de Cadastro: {self.data_cadastro}'
 
-tipos_arquivo = ["CÓPIA", "ORIGINAL", "RASCUNHO", "MINUTA"]
+# Definição da página de login
+layout_login = [
+    [sg.Text('Nome de usuário'), sg.InputText()],
+    [sg.Text('Senha'), sg.InputText(password_char='*')],
+    [sg.Submit(), sg.Cancel()]
+]
 
-documento = Documento("Biblioteca", "A1", "2", "3", "4", "05/02/2023", "5", "2022", "Ministério Público", "Processo Judicial", "Processual", "Petição", "João da Silva", "Nenhuma", tipos_arquivo[1])
+# Criação da janela de login
+window_login = sg.Window('Página de Login', layout_login)
 
-print(documento)
+# Loop de leitura de eventos da página de login
+event, values = window_login.Read()
+
+# Verificação da autenticação
+if event == 'Submit':
+    username = values[0]
+    password = values[1]
+    # Verificar se o nome de usuário e senha estão corretos
+    if username == 'admin' and password == 'admin':
+        # Exibindo a janela de registro de documentos
+        window.Show()
+    else:
+        sg.Popup('Nome de usuário ou senha inválidos')
+else:
+    sg.Popup('Login cancelado')
+
+# Fechando a janela de login
+window_login.Close()
+
+
+
+# Conectando ao banco de dados
+conn = sqlite3.connect('documentos.db')
+cursor = conn.cursor()
+
+# Criando a tabela "documentos" no banco de dados, caso ainda não exista
+table_query = f"""
+CREATE TABLE IF NOT EXISTS documentos (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  local_guarda TEXT,
+  num_prateleira TEXT,
+  num_caixa TEXT,
+  num_dossie TEXT,
+  num_registro TEXT,
+  data_distribuicao TEXT,
+  num_volume TEXT,
+  ano_arquivamento TEXT,
+  proveniencia TEXT,
+  assunto TEXT,
+  especie TEXT,
+  tipologia TEXT,
+  requerente TEXT,
+  observacao TEXT,
+  tipo_arquivo TEXT,
+  data_cadastro TEXT
+)
+"""
+cursor.execute(table_query)
+
+# Definição da GUI
+
+questions = [    'LOCAL DE GUARDA:',    'N° DE PRATELEIRA:',    'N° DE CAIXA:',    'N° DE DOSSIÊ:',    'N° DE REGISTRO:',    'Data de distribuição:',    'Número do volume:',    'Ano do arquivamento:',    'Proveniência:',    'Assunto:',    'Espécie:',    'Tipologia:',    'Requerente:',    'Observação:', 'Tipo do arquivo:', 'Data de cadastro:']
+input_types = [sg.InputText() for _ in range(len(questions) - 1)]
+
+layout = [[sg.Text(q), t] for q, t in zip(questions, input_types)] + [[sg.Submit(), sg.Cancel()]]
+
+# Criação da Janela de exibição
+window = sg.Window('Registro de documentos', layout)
+
+while True:
+    event, values = window.Read()
+    if event == 'Submit':
+        # Inserindo os dados preenchidos na GUI na tabela "documentos" do banco de dados
+        insert_query = f"INSERT INTO documentos ({', '.join(questions)}) VALUES ({', '.join(['?' for _ in questions])})" 
+        cursor.execute(insert_query, tuple(values))
+
+        # Salvando as alterações no banco de dados
+        conn.commit()
+
+        # Exibindo mensagem de sucesso
+        sg.Popup('Registro inserido com sucesso!')
+    if event == 'Cancel' or event is None:
+         # Fechando a GUI
+        break
+    # Fechando a conexão com o banco de dados
+    conn.close()
